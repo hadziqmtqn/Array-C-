b4c6ee62b7f938106b55ffc19d8ec90d2a0d59c9
Hadziq <36812793+hadziqmtqn@users.noreply.github.com>
2019-09-28 10:03:56 +0700

    Add files via upload

---
+++ Array nilai mainimum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int minimum, jumlah, i = 1, lokasi;
+
+  cout << "Masukkan jumlah elemen: ";
+  cin >> jumlah;
+
+  int array[jumlah];
+
+  cout << "Masukkan " << jumlah << " angka\n";
+  for (i = 0; i < jumlah; i ++) {
+    cout << "Elemen ke-" <<(i+1) <<": ";
+    cin >> array[i];
+  }
+
+  minimum = array[0];
+
+  for(i = 0; i < jumlah; i++) {
+    if (array[i] < minimum)  {
+      minimum = array[i];
+      lokasi = i+1;
+    }
+  }
+
+  cout << "Nilai minimum adalah " << minimum << " berada di elemen ke " << lokasi << endl;
+}
